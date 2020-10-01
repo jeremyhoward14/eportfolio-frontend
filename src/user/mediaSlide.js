04b702a2db85cc0da77b82c00702f647a2426995
Kenneth Huynh <khhuy@student.unimelb.edu.au>
2020-10-01 16:21:16 +1000

    Initial implementation of media previewing

---
+++ src/user/mediaSlide.js
@@ -0,0 +1,50 @@
+import React from "react";
+import './mediaSlide.css';
+
+class MediaSlide extends React.Component {
+    constructor(props) {
+        super(props);
+        this.url = props.url;
+        this.index = props.index;
+        this.total = props.total;
+
+        this.MediaDiv = this.MediaDiv.bind(this);
+        this.getFiletype = this.getFiletype.bind(this);
+    }
+
+    getFiletype() {
+        return (this.url).split(/[#?]/)[0].split('.').pop().trim();
+    }
+
+    MediaDiv(){
+        // get filetype
+        const filetype = this.getFiletype();
+
+        // create div based on filetype
+        switch(filetype) {
+            case "jpeg":
+                return(
+                    <img src={this.url} />
+                )
+            case "pdf":
+                return(
+                    <object data={this.url} type="application/pdf">
+                        <embed src={this.url} type="application/pdf" />
+                    </object>
+                )
+        }
+    }
+
+    render() {
+        return (
+            <div className="mediaSlide">
+                <div className="numbertext">{this.index + 1} / {this.total}</div>
+                <div className="media">
+                    <this.MediaDiv />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MediaSlide;
